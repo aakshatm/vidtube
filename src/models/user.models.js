60d7dfd58c7e8694c16fabd75b13f920ac9301a2
aakshatm <aakshatmalhotra100@gmail.com>
2025-06-01 15:36:00 +0530

    JWT ACCESS TOKEN AND REFRESH TOKEN

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -18,7 +19,7 @@ const userSchema = new Schema(
             lowercase: true, 
             trim: true
         }, 
-        fullName: {
+        fullname: {
             type:String, 
             required: true,  
             trim: true, 
@@ -56,6 +57,22 @@ userSchema.pre("save", async function (next){
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    // short lived access jwt token 
+    return jwt.sign({
+        _id: this._id,
+        email: this.email, 
+        username: this.username, 
+        fullname: this.fullname
+    },process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
+}
+userSchema.methods.generateRefreshToken = function (){
+    // short lived access jwt token 
+    return jwt.sign({
+        _id: this._id,
+    },process.env.REFRESH_TOKEN_SECRET, {expiresIn: process.env.REFRESH_TOKEN_EXPIRY})
 }
 
 
