e060842982d2d6e7b2748319e028f91af447422c
aakshatm <aakshatmalhotra100@gmail.com>
2025-06-01 20:43:58 +0530

    user controller

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.models.js"
-import { ApiError } from "../utils/ApiError"
-import { asyncHandler } from "../utils/asyncHandler"
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
